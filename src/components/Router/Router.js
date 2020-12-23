fd86ce4c67478340957252e6264856b416cde46d
Chris <chris.greindl@gmail.com>
2020-12-23 11:51:15 -0300

    Initial commit

---
+++ src/components/Router/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "@/pages/Home";
+import NotFound from "@/pages/NotFound";
+
+const Router = (props) => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
