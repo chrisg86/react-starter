cf5e3fe4944defbae22aceba332879d2cd14a050
Chris <chris.greindl@gmail.com>
2020-12-23 12:08:54 -0300

    Fix prettier

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from "@/pages/Home";
-import NotFound from "@/pages/NotFound";
+import Home from '@/pages/Home';
+import NotFound from '@/pages/NotFound';
 
 const Router = (props) => {
   return (
